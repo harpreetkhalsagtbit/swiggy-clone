c4442e964c1a5db3b1881f66ef6a71cf018c0950
harpreetkhalsagtbit <harpreetkhalsagtbit@gmail.com>
2019-10-11 12:54:35 +0530

    Add: unit test - Jest + React-Testing-Library

---
+++ src/Components/ShowMoreTile/test.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {render, fireEvent} from '@testing-library/react'
+import ShowMoreTile from '../ShowMoreTile/index.js'
+
+describe('<ShowMoreTile /> spec', () => {
+    it('renders the component', () => {
+        render(<ShowMoreTile />)
+        expect(container.firstChild).toMatchSnapshot()
+    })
+    it('assert there are 12 Show More Tiles', () => {
+        render(<ShowMoreTile remainingItems="12"/>)
+ 
+        expect(document.querySelectorAll('.show-more-title span').length).toBe(1)
+        expect(document.querySelector('.show-more-title span').innerHTML).toBe("+12 More")
+    })
+})
